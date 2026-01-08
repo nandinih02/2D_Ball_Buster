422e96b8c84582bb9902a1c4156cc8529e201dee
nandinih02 <79862681+nandinih02@users.noreply.github.com>
2026-01-08 15:11:50 -0500

    Updated Ball Behaviour

--- Assets/Scripts/BallBehaviour.cs
+++ Assets/Scripts/BallBehaviour.cs
@@ -5,33 +5,47 @@ public class BallBehaviour : MonoBehaviour
 {
     public InputAction tap;
     Rigidbody2D rb;
+    CircleCollider2D col;
+
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-    }
-    void OEnable()
-    {
-        tap.Enable();
-    }
-    void ODisable()
-    {
-        tap.Disable();
-        
+        col = GetComponent<CircleCollider2D>();
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if(Camera.main==null)
+        {
+            Debug.Log("Camera Not Found");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Mouse.current != null && Mouse.current.leftButton.IsPressed())
+        {
+            Vector2 mousePos = Mouse.current.position.ReadValue();
+            DestroyOnPress(mousePos);
+
+        }
+        else if(Touchscreen.current !=null && Touchscreen.current.primaryTouch.IsPressed())
+        {
+            Vector2 touchPos = Touchscreen.current.position.ReadValue();
+            DestroyOnPress(touchPos);
+        }
     
     }
-    void OnMouseDown()
+    public void DestroyOnPress(Vector2 position)
     {
-        Destroy(this.gameObject);
+        Ray ray = Camera.main.ScreenPointToRay(position);
+
+        RaycastHit2D hit2D = Physics2D.GetRayIntersection(ray);
+        if(hit2D.collider == col)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
